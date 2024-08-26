65e3d14d19738a84556a67154e20f1645e4a241e
LouisLiu <qingyanjiu@hotmail.com>
2024-08-26 18:10:44 +0800

    update code

--- anda/jk_id_replace.py
+++ anda/jk_id_replace.py
@@ -6,30 +6,21 @@ import os
 
 # ex = pd.read_excel('/Users/louisliu/Downloads/1.监控-all.xlsx', sheet_name=0)
 # ex1 = pd.read_excel('/Users/louisliu/Downloads/监控-新业务id.xlsx', sheet_name=0)
-
 # ids = ex.merge(ex1, how='left', left_on='原始id', right_on='原始id')
 # print(ids)
-
 # e = ex.copy()
 # e['业务主键'] = ids['业务主键_y']
-
 # e.to_excel('/Users/louisliu/Downloads/1.监控.xlsx', index=False)
-
 
 
 
 
 # ex = pd.read_excel('/Users/louisliu/Downloads/1.xlsx', sheet_name=0)
 # ex1 = pd.read_excel('/Users/louisliu/Downloads/2.xlsx', sheet_name=0)
-
 # ids = ex.merge(ex1, how='left', left_on='设备名称', right_on='设备名称')
-
 # e = ex.copy()
-
 # e.loc[e['设备来源']=='智能门锁系统', '业务主键'] = ids['business_key']
-
 # e.to_excel('/Users/louisliu/Downloads/3.xlsx', index=False)
-
 
 
 
@@ -37,21 +28,17 @@ import os
 # ex = pd.read_excel('/Users/louisliu/Downloads/3000.xlsx', sheet_name=0, skiprows=1)
 # ex1 = pd.read_excel('/Users/louisliu/Downloads/wl.xlsx', sheet_name=0)
 # ex2 = pd.read_excel('/Users/louisliu/Downloads/hj.xlsx', sheet_name=0)
-
 # ex1_ids = ex1.merge(ex, how='left', left_on='设备名称', right_on='设备名称')
 # ex2_ids = ex2.merge(ex, how='left', left_on='设备名称', right_on='设备名称')
-
 # e1 = ex1.copy()
 # e2 = ex2.copy()
-
 # e1['空间位置'] = ex1_ids['空间位置_y']
 # e1['原始id'] = e1['原始id'].astype(str)
 # e2['空间位置'] = ex2_ids['空间位置_y']
 # e2['原始id'] = e2['原始id'].astype(str)
-
 # e3 = e1._append(e2)
-
 # e3.to_excel('/Users/louisliu/Downloads/e3.xlsx', index=False)
+
 
 
 # ex = pd.read_excel('/Users/louisliu/Downloads/3.xlsx', sheet_name=0)
@@ -61,15 +48,16 @@ import os
 #     e = ex[offset: offset + 100]
 #     e.to_excel(f'/Users/louisliu/Downloads/t/{i}.xlsx', index=False)
 #     offset += 100
-    
-ex = pd.read_excel('/Users/louisliu/Downloads/wty.xlsx', sheet_name=0)
-ex['name'] = ex['name'].apply(lambda x: 'r-' + x.split('_')[2])
-# ex.to_json('/Users/louisliu/Downloads/eee.xlsx', index=False)
-map = {}
-for index, row in ex.iterrows():
-        map[row.iloc[0]] = row.iloc[1]
 
-mapStr = json.dumps(map, ensure_ascii=False)
-        
-with open('/Users/louisliu/Downloads/eee.json', 'w', encoding='utf-8') as f:
-    f.write(mapStr)
+
+
+# 合并xlsx和json
+ex = pd.read_excel('/Users/louisliu/Downloads/1.xlsx', sheet_name=0)
+ex1 = pd.read_json('/Users/louisliu/Downloads/2.json')
+ex1 = ex1.T
+ex1['code'] = ex1.apply(lambda x: x.name, axis=1)
+ids = ex.merge(ex1, how='inner', left_on='设备名称', right_on='name')
+e = ex.copy()
+e['设备编号'] = ids['code']
+
+e.to_excel('/Users/louisliu/Downloads/3333.xlsx', index=False)
