176d09b20252f675b83b483e53bbbbdf56f4d832
LouisLiu <qingyanjiu@hotmail.com>
2025-05-29 10:51:41 +0800

    更新获取whjuu监控映射关系的代码

---
+++ whjuu/gen_jk_json.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import json
+
+df = pd.read_excel('/Users/louisliu/dev/whjw/芜湖应急摄像头.new.xlsx', sheet_name=0, header=0)
+df.ffill(inplace=True)
+df.drop(columns=['房间', '序号', '数量', 'app'], inplace=True)
+df.rename(columns={'设备': 'name'}, inplace=True)
+df.set_index('stream', inplace=True)
+
+df.to_json('whjuu/jk.json', indent=4, orient='index', index=True, force_ascii=False)
